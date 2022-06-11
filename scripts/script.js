cf7e02c9df04d6e2cdb133b0ce167f2ca00470f5
Woodrow Martyr <woodrow.martyr@gmail.com>
2022-06-12 00:04:25 +0930

    Write pseudocode for 1 game as comments

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,12 @@
-console.log('Hello World')
+console.log('Test');
+
+// create a function called computerPlay
+  // return 'rock', 'paper', or 'scissors' randomly
+
+// create a function called playerSelection
+  // get player choice
+  // return player choice
+
+// create a function called playRound
+  // compare player selection with computer random selection
+  // display result
