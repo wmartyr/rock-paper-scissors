cc0bf4ef3908b8dc3d1bb56a568dc8890a568bb1
Woodrow Martyr <woodrow.martyr@gmail.com>
2022-06-12 08:32:50 +0930

    Refined computerPlay peudocode

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,10 @@ console.log('Test');
 
 // create a function called computerPlay
   // return 'rock', 'paper', or 'scissors' randomly
+    // get a random number between 1 and 3 inclusive
+    // if number is 1, return 'rock
+    // if number is 2, return 'paper'
+    // if number is 3, return 'scissors'
 
 // create a function called playerSelection
   // get player choice
