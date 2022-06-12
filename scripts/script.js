4e7cd4f5590c920317f6889149de948aa1fb8a3d
Woodrow Martyr <woodrow.martyr@gmail.com>
2022-06-12 23:41:43 +0930

    Change code to apply new pseudocode

--- scripts/script.js
+++ scripts/script.js
@@ -23,7 +23,7 @@ function computerPlay() {
 // return formatted answer
 // return "Error" if answer is not valid.
 
-function playerPlay(choice) {
+function formatAnswer(choice) {
   choiceFormatted = choice.charAt(0).toUpperCase() + choice.slice(1).toLowerCase();
   if (choiceFormatted == "Rock" || choiceFormatted == "Paper" || choiceFormatted == "Scissors") {
     return choiceFormatted;
@@ -34,6 +34,7 @@ function playerPlay(choice) {
 
 // create a function called playRound
   // compare playerSelection with computerSelection based on the rules i.e. paper beats rock, rock beats scissors, scissors beats paper
+  // increment score of the winner
   // return result
 
 function playRound(playerSelection, computerSelection) { 
@@ -43,33 +44,33 @@ function playRound(playerSelection, computerSelection) {
       break;
     case "Rock":
       if (computerSelection == "Paper") {
+        computerScore++;
         return "You lose! Paper beats Rock.";
       } else {
+        playerScore++;
         return "You win! Rock beats Scissors";
       }
       break;
     case "Paper":
       if (computerSelection == "Rock") {
+        playerScore++;
         return "You win! Paper beats Rock.";
       } else {
+        computerScore++;
         return "You lose! Scissors beats Paper";
       }
       break;
     case "Scissors":
       if (computerSelection == "Rock") {
+        computerScore++;
         return "You lose! Rock beats Scissors";
       } else {
+        playerScore++;
         return "You win! Scissors beats Paper"
       }
       break;
   }
 }
-
-// create a variable named playerSelection.
-// create a variable named computerSelection.
-let playerSelection;
-let computerSelection;
-let resultMessage;
 
 // create a function named game
   // create a variable named playerScore with an initial value of 0 that would keep track of player's score.
@@ -77,10 +78,38 @@ let resultMessage;
   // create a loop that would run 5 times
     // run computerPlay and save in computerSelection
     // prompt user for choice.
-    // run playerPlay to format choice.
+    // run formatAnswer to format choice.
     // if choice is in the wrong format, prompt user again.
     // save player's choice in playerSelection.
     // run playRound
     // display result of round
-    // increment the score of the winner.
   // determine the final winner and display the final result.
+
+function game() {
+  playerSelection = "Error";
+  for (let i = 0; i < 5; i++) {
+    computerSelection = computerPlay();
+    while (playerSelection == "Error") {
+      choice = prompt("Enter a choice between Rock, Paper, or Scissors");
+      playerSelection = formatAnswer(choice);
+    }
+    console.log(playRound(playerSelection, computerSelection));
+  }
+  if (playerScore > computerScore) {
+    console.log("Congratulations! You won " + playerScore + " to " + computerScore + "!")
+  } else {
+    console.log ("Better luck next time. You lost " + computerScore + " to " + playerScore + "!")
+  }
+}
+
+// create a variable named playerSelection.
+// create a variable named computerSelection.
+// create a variable named choice
+// create a variable named playerScore and set to 0;
+// create a variable named computerScore and set to 0;
+let playerSelection;
+let computerSelection;
+let choice;
+let playerScore = 0;
+let computerScore = 0;
+game();
